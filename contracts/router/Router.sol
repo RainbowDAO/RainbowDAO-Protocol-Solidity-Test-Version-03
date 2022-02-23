4a954d332fde52f47c029870aea23b1254be445b
ivanvian <ivanvian@yeah.net>
2022-02-23 15:45:07 +0800

    _

--- contracts/router/Router.sol
+++ contracts/router/Router.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.8.0;
 
-contract Router {
+import "../dao/Dao.sol";
 
+contract Router {
+        /**
+        *create A dao
+        */
+        function createDao() public{
+             Dao   dao = new Dao();
+        }
 }
